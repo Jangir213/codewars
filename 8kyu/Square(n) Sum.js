adb04c1c9ba710ba8f6ff2925a7682f8facf41da
jangir <jangirhus@gmail.com>
2016-12-04 19:01:51 +0200

    add

---
+++ 8kyu/Square(n) Sum.js
@@ -0,0 +1,11 @@
+/*
+Description:
+  Complete the squareSum method so that it squares each number passed into it and then sums the results together.
+
+For example:
+  squareSum([1, 2, 2]); // should return 9
+*/
+
+function squareSum(numbers){
+   return numbers.reduce((a, b) => a + b * b, 0);
+}
